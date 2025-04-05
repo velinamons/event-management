6bcb664cb215e94e175de902498877dc695dd2cf
velinamons <velinamons@gmail.com>
2025-04-05 16:11:57 +0300

    Minor fix: core/urls.py

--- core/urls.py
+++ core/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     # path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     # path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
-    path('api/users/', include('users.urls')),]
+    path('api/users/', include('users.urls')),
+]
