32ea45a93e4cb4ad4f3419441a3a38f40d11d7e0
velinamons <velinamons@gmail.com>
2025-04-05 20:12:07 +0300

    Create Event model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateTimeField()
+    location = models.CharField(max_length=255)
+    organizer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="organized_events")
+    is_registration_active = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return f"{self.title} - {self.date}"
