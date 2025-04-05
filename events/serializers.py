ac1ad29f2e3d3abdba5281d0de6253d3702e5ad0
velinamons <velinamons@gmail.com>
2025-04-05 20:13:44 +0300

    Add EventReadSerializer and EventWriteSerializer with extra fields and date validation

---
+++ events/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from django.utils import timezone
+
+from users.serializers import UserSerializer
+from .models import Event
+
+
+class EventReadSerializer(serializers.ModelSerializer):
+    organizer = UserSerializer()
+
+    class Meta:
+        model = Event
+        fields = ["id", "title", "description", "date", "location", "organizer", "is_registration_active"]
+
+
+class EventWriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = ["title", "description", "date", "location", "is_registration_active"]
+
+    def create(self, validated_data):
+        validated_data["organizer"] = self.context["request"].user
+        event = super().create(validated_data)
+        return event
+
+    def validate(self, attrs):
+        extra_fields = set(attrs.keys()) - set(self.Meta.fields)
+        if extra_fields:
+            raise serializers.ValidationError(f"Invalid fields: {', '.join(extra_fields)}")
+        return attrs
+
+    def validate_date(self, value):
+        if value <= timezone.now():
+            raise serializers.ValidationError("The event date must be in the future.")
+        return value
