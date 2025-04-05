5be0b945bc2c36aafcec66cb778de354e9dcf123
velinamons <velinamons@gmail.com>
2025-04-05 15:59:04 +0300

    Create User model; Add UserSerializer and RegisterSerializer with create() method

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(_("email address"), unique=True, blank=False)
+    first_name = models.CharField(_("first name"), max_length=50, blank=False)
+    last_name = models.CharField(_("last name"), max_length=50, blank=False)
+
+    class Meta(AbstractUser.Meta):
+        swappable = "AUTH_USER_MODEL"
+
+    def __str__(self):
+        return self.username
---
+++ users/serializers.py
@@ -0,0 +1,29 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "first_name", "last_name"]
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "first_name", "last_name", "password"]
+
+    def create(self, validated_data: dict) -> User:
+        user = User(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            first_name=validated_data["first_name"],
+            last_name=validated_data["last_name"],
+        )
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
