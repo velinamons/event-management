cc023eab6c6181ca72c0d3b83cb0e64c8a7ce72e
velinamons <velinamons@gmail.com>
2025-04-06 20:20:15 +0300

    Create EventRegistration model

--- events/models.py
+++ events/models.py
@@ -14,3 +14,15 @@ class Event(models.Model):
 
     def __str__(self) -> str:
         return f"{self.title} - {self.date}"
+
+
+class EventRegistration(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="event_registrations")
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="registrations")
+    registration_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("user", "event")
+
+    def __str__(self) -> str:
+        return f"Registration for {self.user.username} -> {self.event.title}"
