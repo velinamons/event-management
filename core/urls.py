98f8533f74de46a0c726809f9ce059c460083e3f
velinamons <velinamons@gmail.com>
2025-04-08 20:16:13 +0300

    Minor fix: redundant comments

--- core/urls.py
+++ core/urls.py
@@ -1,15 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    # path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    # path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path('api/users/', include('users.urls')),
     path('api/events/', include('events.urls')),
 
