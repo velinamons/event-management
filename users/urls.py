7de8a3486a3a2c1a8dbdbc60e2fe0d201d29715a
velinamons <velinamons@gmail.com>
2025-04-08 15:12:34 +0300

    Minor fix: redundant comment

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("", ListUsersView.as_view(), name="user-list"),
     path("<int:pk>/", RetrieveUserView.as_view(), name="user-retrieve"),
-    # urls.py (in users/urls.py)
     path("me/", MeView.as_view(), name="me"),
     path("me/registrations/", MyEventRegistrationsView.as_view(), name="my-registrations"),
 ]
